432184a6f7b172e828d4da78b17c2c854d5ab1e6
CAO23313 <qyu46@ucsc.edu>
2025-09-30 12:40:31 -0700

    lecture 1 journal update

---
+++ lecture 1/lec 1.py
@@ -0,0 +1,14 @@
+
+name = "Eric Yu"
+pronous = "He/Him"
+sentence_1 = "My favorite movie is Godfather"
+sentence_2 = "my favorite foood is Chinese food"
+
+def printInformation():
+    print(name + "\n" + pronous + "\n" + sentence_1 + "\n" + sentence_2)
+
+def main():
+    printInformation()
+
+if __name__ == "__main__":
+    main()
